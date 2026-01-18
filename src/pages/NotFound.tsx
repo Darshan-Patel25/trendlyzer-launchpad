05077432acffe31b360141a8fd3361fc85296970
Darshan Patel <darshanpatel29200@gmail.com>
2026-01-18 13:01:42 +0530

    final commit

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -15,6 +15,8 @@ const NotFound = () => {
         <p className="mb-4 text-xl text-muted-foreground">Oops! Page not found</p>
         <a href="/" className="text-primary underline hover:text-primary/90">
           Return to Home
+
+          Thank You
         </a>
       </div>
     </div>
